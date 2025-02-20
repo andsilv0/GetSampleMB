76907abd04086d7890b4ef2ca31641ccc836e94f
nltt-br <nlargato@gmail.com>
2025-02-20 09:27:05 -0300

    💥 Ajustado README para refletir a realidade para utilização e ajustado o código para simplificar ainda mais a utilização, validado as funções de busca na base.

--- GetSampleMB.py
+++ GetSampleMB.py
@@ -16,10 +16,10 @@ from colorama import init, Fore, Back, Style
 init()
 
 class GetSample():
-    def __init__(self, tag, limit=2, option=False):
-        self.tag = tag
+    def __init__(self, limit=2, option=False):
         self.limit = limit
         self.option = option
+        
 
     def generate_file_name(self):
         length = 12
@@ -42,7 +42,7 @@ class GetSample():
     """
         print(Fore.GREEN + f'{var}')
 
-    def find_for_tag(self):
+    def list_for_tag(self, tag):
         print('0 - Definir variáveis locais')
         capture_values, saves, data = {}, {}, {}
 
@@ -57,7 +57,7 @@ class GetSample():
 
         if self.limit > 0:
             data = {'query': 'get_taginfo',
-                    'tag': self.tag,
+                    'tag': tag,
                     'limit': self.limit}
 
         try:
@@ -90,18 +90,125 @@ class GetSample():
         except Exception as e:
             print('Error in {}'.format(e))    
 
+    def list_for_filetype(self, file_type):
+        print('0 - Definir variáveis locais')
+        capture_values, saves, data = {}, {}, {}
+
+        headers = {
+            'Auth-Key': getenv('AUTH'),
+            'User-Agent': 'Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41'
+        }
+
+        url = 'https://mb-api.abuse.ch/api/v1/'
+
+        current_path = getcwd()
+
+        if self.limit > 0:
+            data = {'query': 'get_file_type',
+                    'file_type': file_type,
+                    'limit': self.limit}
+
+        try:
+            print('1 - Requisição para o MalwareBaazar')
+            req = post(url, data=data, headers=headers)
+
+            if req.status_code == 200:
+                saves = req.json()
+                print('2 - Laço de repetição para armazenar os valores encontrados')
+                for save in saves['data']:
+                    file_name = save['file_name']
+                    obj = {'file_name': save['file_name'],
+                        'file_size': save['file_size'],
+                        'file_type': save['file_type'],
+                        'signature': save['signature'],
+                        'reporter': save['reporter'],
+                        'sha256_hash': save['sha256_hash']}
+                    
+                    capture_values[file_name] = obj
+                
+                print('3 - Printando os valores encontrados')
+                print(dumps(capture_values, indent=4))
+
+                if self.option:
+                    file_name=self.generate_file_name()
+                    print('4 - Baixando os valores encontrados na pasta output')
+                    with open(f'{current_path}/output/{file_name}.json', 'a') as f:
+                        f.write(dumps(capture_values, ensure_ascii=False, indent=4))
+
+        except Exception as e:
+            print('Error in {}'.format(e))   
+
+    def list_for_hash(self, hash):
+            print('0 - Definir variáveis locais')
+            capture_values, saves, data = {}, {}, {}
+
+            headers = {
+                'Auth-Key': getenv('AUTH'),
+                'User-Agent': 'Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41'
+            }
+
+            url = 'https://mb-api.abuse.ch/api/v1/'
+
+            current_path = getcwd()
+
+            if self.limit > 0:
+                data = {'query': 'get_info',
+                        'hash': hash,
+                        'limit': self.limit}
+
+            try:
+                print('1 - Requisição para o MalwareBaazar')
+                req = post(url, data=data, headers=headers)
+
+                if req.status_code == 200:
+                    saves = req.json()
+                    print('2 - Laço de repetição para armazenar os valores encontrados')
+                    for save in saves['data']:
+                        file_name = save['file_name']
+                        obj = {'file_name': save['file_name'],
+                            'file_size': save['file_size'],
+                            'file_type': save['file_type'],
+                            'signature': save['signature'],
+                            'reporter': save['reporter'],
+                            'sha256_hash': save['sha256_hash']}
+                        
+                        capture_values[file_name] = obj
+                    
+                    print('3 - Printando os valores encontrados')
+                    print(dumps(capture_values, indent=4))
+
+                    if self.option:
+                        file_name=self.generate_file_name()
+                        print('4 - Baixando os valores encontrados na pasta output')
+                        with open(f'{current_path}/output/{file_name}.json', 'a') as f:
+                            f.write(dumps(capture_values, ensure_ascii=False, indent=4))
+
+            except Exception as e:
+                print('Error in {}'.format(e))   
+
+
 def main():
-    parser = ArgumentParser(description="Busca amostras de malware por tag")
+    parser = ArgumentParser(description="Busca amostras e faz download de malware")
     
-    parser.add_argument("tag", type=str, help="Nome da tag para busca (exemplo: TrickBot)")
-    parser.add_argument("-l", "--limit", type=int, default=2, help="Linhas de busca")
+    parser.add_argument("-t", "--tag", type=str, nargs="?", help="Se ativa a opção, é possível pesquisar por tag (exemplo: TrickBot)")
+    parser.add_argument("-l", "--limit", type=int, default=2, help="Quantidade de linhas para a consulta, padrão 2")
+    parser.add_argument("-f", "--file_type", type=str, help="Se ativa a opção, é possível pesquisar por extensão do arquivo")
+    parser.add_argument("-ha", "--hash", type=str, help="Se ativa a opção, é possível pesquisar por hash do arquivo")
     parser.add_argument("-o", "--option", action="store_true", help="Habilita o download do resultado em formato json")
 
     args = parser.parse_args()
 
-    GetSampleObj = GetSample(args.tag, args.limit, args.option)
+    GetSampleObj = GetSample(args.limit, args.option)
     GetSampleObj.banner()
-    GetSampleObj.find_for_tag()
+
+    if args.file_type:
+        GetSampleObj.list_for_filetype(args.file_type)
+
+    if args.tag:
+        GetSampleObj.list_for_tag(args.tag)
+
+    if args.hash:
+        GetSampleObj.list_for_hash(args.hash)
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -5,6 +5,17 @@
 ## Usage
 
 ```
+---
+
+[0] - Create auth_key -> https://auth.abuse.ch/
+
+[00] - Create file .env with variable in current directory 
+
+[1] - Example:
+  AUTH='69d00b49efaffec4821af2e72039803b575d4c26c7e11b1e1f1dc59c6ee926cf'
+
+--------
+
 usage: GetSampleMB.py [-h] [-l LIMIT] [-o] tag
 
 Busca amostras de malware por tag
@@ -23,3 +34,21 @@ options:
 ## Example
 
 `python GetSampleMB.py TrickBot -l 1`
+
+
+## Limitations 
+
+```
+Download limit on the file download API
+
+MalwareBazaar runs on Google Cloud infrastructure. Sadly, network egress traffic from Google Cloud is extremely expensive. We therefore had to restrict the number of file downloads on our file download API to 2,000 per IP address/day. For bulk downloads we recommend you to use the hourly and daily file exports of MalwareBazaar served by our datalake:
+
+    MalwareBazaar hourly malware batches (ZIP password: infected)
+    MalwareBazaar daily malware batches (ZIP password: infected)
+
+Should you have valid reasons to download more than 2,000 malware samples through the file download API per day, feel free to reach out to us using the Spamhaus Technology contact form:
+https://www.spamhaus.com/#contact-form
+
+https://bazaar.abuse.ch/faq/#api-limit
+
+```
